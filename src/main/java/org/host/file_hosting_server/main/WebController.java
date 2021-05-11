8ac21c68c88fa5980ec529fd552ecdbc412f0a2b
origin <chldlsrb1000@naver.com>
2021-05-11 16:33:51 +0900

    index 페이지 추가

--- src/main/java/org/host/file_hosting_server/main/WebController.java
+++ src/main/java/org/host/file_hosting_server/main/WebController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    @GetMapping("/")
+    public String index() {return "index";}
     @GetMapping("/mit09")
     public String mit09() {return "mit09";}
 }
